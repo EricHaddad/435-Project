730f892464e1ac51918124313c00948ea6d5fbce
vuthuynhi <vuthuynhi05@gmail.com>
2023-11-12 19:54:46 -0600

    finished BucketsortCUDA

--- BucketsortCUDA/bucketSortCUDA.cu
+++ BucketsortCUDA/bucketSortCUDA.cu
@@ -1,14 +1,53 @@
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
 #include <stdio.h>
-#include <cuda.h>
+#include <time.h>
+
 #include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
+#include <cuda_runtime.h>
+#include <cuda.h>
 
 #define RANGE 100
 
-// CUDA kernel for bucket sort
-__global__ void bucket_sort(int *input, int *buckets, int n) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if(idx < n) {
-        atomicAdd(&buckets[input[idx]], 1);
+void print_elapsed(clock_t start, clock_t stop)
+{
+  double elapsed = ((double) (stop - start)) / CLOCKS_PER_SEC;
+  printf("Whole computation time: %.3fs\n", elapsed);
+}
+
+// generate input types
+void generateInput(int *input, int size, char* type) {
+    if (strcmp(type, "sorted") == 0) {
+        for (int i = 0; i < size; i++) {
+            input[i] = i;
+        }
+    } else if (strcmp(type, "random") == 0) {
+        srand(time(NULL));
+        for (int i = 0; i < size; i++) {
+            input[i] = rand() % RANGE;
+        }
+        printf("\n");
+    } else if (strcmp(type, "reverse") == 0) {
+        for (int i = 0; i < size; i++) {
+            input[i] = size - i;
+        }
+        printf("\n");
+    } else if (strcmp(type, "perturbed") == 0) {
+        for (int i = 0; i < size; i++) {
+            input[i] = i;
+        }
+        // Perturb 1% of the elements
+        int perturbCount = size / 100;
+        srand(time(NULL));
+        for (int i = 0; i < perturbCount; i++) {
+            // Choose random index to perturb
+            int idx = rand() % size;
+            // Perturb the value
+            input[idx] = rand() % RANGE;
+        }
     }
 }
 
@@ -26,12 +65,17 @@ void correctness_check(int *array, int size) {
     CALI_MARK_END("correctness_check");
 }
 
+// CUDA kernel for bucket sort
+__global__ void bucket_sort(int *input, int *buckets, int n) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if(idx < n) {
+        atomicAdd(&buckets[input[idx]], 1);
+    }
+}
+
 void bucketSort(int* input, int* output, int size, int blockSize) {
     // Allocate device memory
     int *d_input, *d_buckets;
-
-    cudaEvent_t start, stop;
-    float gpu_time = 0.0f;
 
     cudaMalloc(&d_input, size * sizeof(int));
     cudaMalloc(&d_buckets, RANGE * sizeof(int));
@@ -48,9 +92,6 @@ void bucketSort(int* input, int* output, int size, int blockSize) {
     dim3 dimBlock(blockSize);
     dim3 dimGrid((size + dimBlock.x - 1) / dimBlock.x);
 
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start, 0);
 
     // Call the kernel
     CALI_MARK_BEGIN("comp");
@@ -59,10 +100,6 @@ void bucketSort(int* input, int* output, int size, int blockSize) {
     CALI_MARK_END("comp_large");
     CALI_MARK_END("comp");
     
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&gpu_time, start, stop);
-    printf("GPU time: %.4f milliseconds\n", gpu_time);
 
     // Copy the buckets back to the host
     CALI_MARK_BEGIN("comm");
@@ -75,8 +112,15 @@ void bucketSort(int* input, int* output, int size, int blockSize) {
     // Generate the sorted array
     int pos = 0;
     for(int i = 0; i < RANGE; ++i) {
+        std::vector<int> bucket;
         for(int j = 0; j < buckets[i]; ++j) {
-            output[pos++] = i;
+            bucket.push_back(i);
+        }
+        // Sort each bucket
+        std::sort(bucket.begin(), bucket.end());
+        // Add sorted bucket to output
+        for (int j = 0; j < bucket.size(); ++j) {
+            output[pos++] = bucket[j];
         }
     }
 
@@ -92,26 +136,62 @@ int main(int argc, char *argv[]) {
 
     blockSize = atoi(argv[1]);
     size = atoi(argv[2]);
+    char *input_type = argv[3];
 
     int *input = new int[size];
     int *output = new int[size];
 
+    // Initialize data with user input type's values
     CALI_MARK_BEGIN("data_init");
-    // Generate random input
-    srand(time(NULL));
-    for(int i = 0; i < size; i++) {
-        input[i] = rand() % RANGE;
-    }
+    generateInput(input, size, input_type);
     CALI_MARK_END("data_init");
 
-
+    clock_t start, end;
+    start = clock();
     bucketSort(input, output, size, blockSize);
+    end = clock();
+
+    print_elapsed(start, end);
 
     correctness_check(output, size);
 
     // Clean up
     delete[] input;
     delete[] output;
+
+    // Record metadata with Adiak
+    std::string algorithm = "BucketSort"; // replace with your algorithm name
+    std::string programmingModel = "CUDA";
+    std::string datatype = "int"; // replace with your data type
+    int sizeOfDatatype = sizeof(int); // replace with your data type size
+    int inputSize = size; // replace with your input size
+    std::string inputType = input_type; // replace with your input type
+    int num_threads = blockSize; // replace with your number of threads
+    int num_blocks = (size + num_threads - 1) / num_threads; // replace with your number of CUDA blocks
+    int group_number = 1; // replace with your group number
+    std::string implementation_source = "Handwritten"; // replace with your source type
+
+    printf("THREADS: %d\n", num_threads);
+    printf("NUM_VALS: %d\n", inputSize);
+    printf("BLOCKS: %d\n", num_blocks);
+    printf("Input Type: %s\n", input_type);
+
+    // Initialize Adiak
+    adiak::init(NULL);
+    adiak::launchdate();
+    adiak::libraries();
+    adiak::cmdline();
+    adiak::clustername();
+    adiak::value("Algorithm", algorithm);
+    adiak::value("ProgrammingModel", programmingModel);
+    adiak::value("Datatype", datatype);
+    adiak::value("SizeOfDatatype", sizeOfDatatype);
+    adiak::value("InputSize", inputSize);
+    adiak::value("InputType", inputType);
+    adiak::value("num_threads", num_threads);
+    adiak::value("num_blocks", num_blocks);
+    adiak::value("group_num", group_number);
+    adiak::value("implementation_source", implementation_source);
 
     CALI_MARK_END("main");
     return 0;
